c6d57f08bd70be955a2a2937579297f52bf930f0
PetkoGenchev <petko.genchev@yahoo.com>
2022-09-16 13:38:23 +0300

    Adding Views

---
+++ CarRentingSystem/Controllers/CarsController.cs
@@ -0,0 +1,17 @@
+﻿namespace CarRentingSystem.Controllers
+{
+    using CarRentingSystem.Models.Cars;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CarsController : Controller
+    {
+        public IActionResult Add() => View();
+
+        [HttpPost]
+        public IActionResult Add(AddCarFormModel car)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ CarRentingSystem/Models/Cars/AddCarFormModel.cs
@@ -0,0 +1,17 @@
+﻿namespace CarRentingSystem.Models.Cars
+{
+    public class AddCarFormModel
+    {
+        public string Brand { get; init; }
+
+        public string Model { get; init; }
+
+        public string Description { get; init; }
+
+        public string ImageUrl { get; init; }
+
+        public int Year { get; init; }
+
+        public int CategoryID { get; init; }
+    }
+}
