7828d9ce934c15a4158511c2d85b30d13b4ffb19
PetkoGenchev <petko.genchev@yahoo.com>
2022-09-21 16:19:26 +0300

    Edit Home Controller

---
+++ CarRentingSystem/Models/Cars/AllCarsQueryModel.cs
@@ -0,0 +1,16 @@
+﻿namespace CarRentingSystem.Models.Cars
+{
+    using System.Collections.Generic;
+
+    public class AllCarsQueryModel
+    {
+        public IEnumerable<string> Brands { get; init; }
+
+        public IEnumerable<string> SearchTerm { get; init; }
+
+        public CarSorting Sorting { get; init; }
+
+        public IEnumerable<CarListingViewModel> Cars { get; init; }
+
+    }
+}
